1e3fe70325d2f317bafd413c0c035bceeda91c42
gemelinvest <oriya.som@gmail.com>
2026-05-27 13:40:19 +0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,9 @@
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+// No fetch handler: an empty listener triggers a Chrome "no-op fetch" warning without benefit.
