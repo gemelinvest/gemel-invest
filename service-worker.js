ab968967bda054060d9bc9b0bcafedc8e33711de
gemelinvest <oriya.som@gmail.com>
2026-07-11 01:21:27 +0300

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,9 +0,0 @@
-self.addEventListener('install', (event) => {
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', (event) => {
-  event.waitUntil(self.clients.claim());
-});
-
-// No fetch handler: an empty listener triggers a Chrome "no-op fetch" warning without benefit.
