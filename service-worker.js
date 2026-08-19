d995df56ed39faf6a7491011c3089cf74c055123
gemelinvest <oriya.som@gmail.com>
2026-08-20 02:07:20 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -16,7 +16,7 @@
    לאיפוס ידני: כפתור "החל עדכון" במערכת כבר מוחק את כל המטמונים ומבטל רישום SW.
 */
 
-const CACHE_VERSION = "gi-v12-20260820-issued-upload-admin-v1";
+const CACHE_VERSION = "gi-v12-20260820-welcome-white-v1";
 const RUNTIME_CACHE = `gi-runtime-${CACHE_VERSION}`;
 
 // סיומות שמותר להגיש מהמטמון.
